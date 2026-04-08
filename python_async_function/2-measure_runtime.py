422952a0601eeda88530cc013051fe14c8a5518c
tomvieilledent <tomvieilledent@gmail.com>
2026-04-08 10:57:06 +0200

    Add docstrings to measure_time function for clarity

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Measure runtime of concurrent coroutines."""
 
 import time
 
@@ -6,6 +7,7 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 async def measure_time(n: int, max_delay: float) -> float:
+    """Measure average execution time for wait_n coroutines."""
     start = time.time()
     await wait_n(n, max_delay)
     end = time.time()
