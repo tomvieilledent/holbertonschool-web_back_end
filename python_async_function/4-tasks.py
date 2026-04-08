563376caa75b17f6d5bd5a725c7980ae05eb921c
tomvieilledent <tomvieilledent@gmail.com>
2026-04-08 13:04:51 +0200

    Add initial implementation of task_wait_n and measure_time functions

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Measure runtime of concurrent coroutines."""
+
+import asyncio
+import time
+
+task_wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Measure average execution time for wait_n coroutines."""
+    start = time.time()
+    asyncio.run(task_wait_n(n, max_delay))
+    end = time.time()
+    total_time = end - start
+    return total_time / n
