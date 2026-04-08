cd87d843e6b075c69358db8abb1de8915d828305
tomvieilledent <tomvieilledent@gmail.com>
2026-04-08 13:53:32 +0200

    Add async_generator and main script for yielding random floats

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''Async Generator'''
+
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    '''Yield 10 random floats.'''
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
