ce1eefe8834acfd3dcf36e500fd032ca4a340bdc
tomvieilledent <tomvieilledent@gmail.com>
2026-04-08 09:15:32 +0200

    Fix type annotation for input_list in sum_list function

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Sum list of floats."""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Return sum of all floats in list."""
     result = sum(input_list)
     return result
