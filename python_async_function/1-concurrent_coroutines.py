e85d7f8d0bdc74cdf3daba4d1c522db836747303
tomvieilledent <tomvieilledent@gmail.com>
2026-04-08 10:22:30 +0200

    Add concurrent coroutines implementation and test script

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Concurrent coroutines."""
+
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawn wait_random n times and return delays in ascending order."""
+    coroutines = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*coroutines)
+    return sorted(delays)
