03aad2126f24e519b526ec28dba3c0459d00a83a
tomvieilledent <tomvieilledent@gmail.com>
2026-04-08 11:18:57 +0200

    Refactor measure_time function to use asyncio.run and remove async keyword

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 """Measure runtime of concurrent coroutines."""
 
+import asyncio
 import time
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: float) -> float:
+def measure_time(n: int, max_delay: float) -> float:
     """Measure average execution time for wait_n coroutines."""
     start = time.time()
-    await wait_n(n, max_delay)
+    asyncio.run(wait_n(n, max_delay))
     end = time.time()
     total_time = end - start
     return float(total_time / n)
