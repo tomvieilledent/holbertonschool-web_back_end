721b12b72263a98f8820189134b19084d02c27f6
tomvieilledent <tomvieilledent@gmail.com>
2026-04-08 15:11:03 +0200

    Fix typo in type hint for async_generator function

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generaton[float, None, None]: # type: ignore
+async def async_generator() -> Generator[float, None, None]: # type: ignore
     '''Yield 10 random floats. Cest trop court donc j'en rajoute'''
     for _ in range(10):
         await asyncio.sleep(1)
