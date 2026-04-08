101c1422b4f40e7ffd9a8ecb6a0afe662851182a
tomvieilledent <tomvieilledent@gmail.com>
2026-04-08 14:35:18 +0200

    Refactor async_generator to use AsyncGenerator type hint and update docstring for clarity

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
-'''Async Generator'''
+"""Module for asynchronous generator coroutines."""
 
 
 import asyncio
 import random
-from typing import Generator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> Generator[float, None, None]:
-    '''Yield a float between 0 and 1O, ten times'''
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Coroutine that yields random floats asynchronously.
+    
+    Loops 10 times, waiting 1 second each iteration before yielding
+    a random float between 0 and 10.
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
-        
+
