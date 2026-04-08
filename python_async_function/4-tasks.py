2a01c4d3c666db991c22f7b695d00a2d5883012a
tomvieilledent <tomvieilledent@gmail.com>
2026-04-08 13:09:27 +0200

    Refactor task_wait_n function to spawn task_wait_random and return sorted delays

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
-"""Measure runtime of concurrent coroutines."""
+"""Create asyncio tasks."""
 
 import asyncio
-import time
+from typing import List
 
-task_wait_n = __import__('1-concurrent_coroutines').wait_n
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-def measure_time(n: int, max_delay: int) -> float:
-    """Measure average execution time for wait_n coroutines."""
-    start = time.time()
-    asyncio.run(task_wait_n(n, max_delay))
-    end = time.time()
-    total_time = end - start
-    return total_time / n
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawn task_wait_random n times and return delays in ascending order."""
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
