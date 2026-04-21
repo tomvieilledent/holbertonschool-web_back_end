bd5686ba7ef5b0b7d768b8edccd170374bac5de9
tomvieilledent <tomvieilledent@gmail.com>
2026-04-21 15:58:25 +0200

    Refactor update_topics function to use update_many and improve docstring

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
 
 def update_topics(mongo_collection, name, topics):
-    return mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
+    """Update topics with topics list"""
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
