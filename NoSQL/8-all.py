63e8cfd8b32ee86415e6be80aa75cdb4c3595a8f
tomvieilledent <tomvieilledent@gmail.com>
2026-04-21 15:30:50 +0200

    Refactor docstring in list_all function to simplify and clarify usage

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -3,15 +3,7 @@
 
 
 def list_all(mongo_collection):
-    """Return a list of all documents in a MongoDB collection.
-
-    Args:
-            mongo_collection: pymongo collection object.
-
-    Returns:
-            A list of documents. Returns an empty list when the collection
-            has no documents.
-    """
+    """Return a list of all documents in a MongoDB collection."""
     if mongo_collection is None:
         return []
     return list(mongo_collection.find())
