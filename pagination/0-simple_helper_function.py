a0e15fdfb4af793c3621de9867c36628a6a81cd2
tomvieilledent <tomvieilledent@gmail.com>
2026-04-21 09:15:59 +0200

    Add docstring to index_range function and improve module documentation

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+"""Pagination utility module."""
+
 
 def index_range(page, page_size) -> tuple[int, int]:
+    """Return start and end indices for a given page."""
     start = (page - 1) * page_size
     end = page * page_size
     return (start, end)
