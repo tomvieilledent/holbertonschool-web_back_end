83ab5a1b575711e30b93db8cf094332e6c19c967
tomvieilledent <tomvieilledent@gmail.com>
2026-04-30 08:55:31 +0200

    Fix typos in Currency class error messages and add Pricing class for price management

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -10,7 +10,7 @@ export default class Currency {
 
   set code(value) {
     if (typeof value !== 'string') {
-      throw new TypeError('Code muste be a string');
+      throw new TypeError('Code must be a string');
     }
 
     this._code = value;
@@ -22,7 +22,7 @@ export default class Currency {
 
   set name(value) {
     if (typeof value !== 'string') {
-      throw new TypeError('Name muste be a string');
+      throw new TypeError('Name must be a string');
     }
 
     this._name = value;
---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,30 @@
+import Currency from "./3-currency.js";
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('Currency must be a Currency instance');
+    }
+    this._currency = value;
+  }
+}
