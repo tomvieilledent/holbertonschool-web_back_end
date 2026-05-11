3e54af46b8afd103f95231a2c8bac91def45c25c
tomvieilledent <tomvieilledent@gmail.com>
2026-05-11 14:12:34 +0200

    fix: reorder response handling in HTTP server for clarity and consistency

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -2,10 +2,12 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
   if (req.url === '/') {
     res.end('Hello Holberton School!');
-  }
-  if (req.url === '/students') {
+  } else if (req.url === '/students') {
     const database = process.argv[2];
     countStudents(database)
       .then((output) => {
@@ -14,6 +16,9 @@ const app = http.createServer((req, res) => {
       .catch((error) => {
         res.end(`This is the list of our students\n${error.message}`);
       });
+  } else {
+    res.statusCode = 404;
+    res.end('Invalid request');
   }
 });
 
